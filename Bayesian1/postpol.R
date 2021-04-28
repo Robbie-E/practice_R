7931755e3ebd97700cd4031b2899bfeed3650e4a
Robertson Esperanza <33261713+Robbie-E@users.noreply.github.com>
2021-04-28 12:53:42 +0800

    Update postpol.R

--- Bayesian1/postpol.R
+++ Bayesian1/postpol.R
@@ -2,6 +2,7 @@ post.pol <- function(pref, yeses, cprior=0.5) {
   #pref is guess of political preference, i.e. con or lib
   #yeses is no.of yes in the questionnaire
   #cprior is unconditional probability of being con
+  #output is P(preference|yeses)
   normf <- (dbinom(yeses,size=5,prob=0.8)*cprior)+(dbinom(yeses,size=5,prob=0.3)*(1-cprior))
   if(identical(pref,'con')){
     post <- (dbinom(yeses,size=5,prob=0.8)*cprior)/normf
